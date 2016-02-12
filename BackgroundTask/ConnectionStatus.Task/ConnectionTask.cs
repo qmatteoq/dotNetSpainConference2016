447fa42b98295bf15d19573cd00da60536552c0a
Matteo Pagani <info@qmatteoq.com>
2016-02-13 00:35:02 +0100

    Added standard background task sample

---
+++ BackgroundTask/ConnectionStatus.Task/ConnectionTask.cs
@@ -0,0 +1,33 @@
+﻿using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.Networking.Connectivity;
+using Windows.UI.Notifications;
+
+namespace ConnectionStatus.Task
+{
+    public sealed class ConnectionTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var connectionProfile = NetworkInformation.GetInternetConnectionProfile();
+            NetworkConnectivityLevel level = connectionProfile.GetNetworkConnectivityLevel();
+            var message = level == NetworkConnectivityLevel.InternetAccess ? "Connected to Internet" : "Not connected to Internet";
+
+            string xml = $@"
+            <toast activationType='foreground' launch='args'>
+                <visual>
+                    <binding template='ToastGeneric'>
+                        <text>{message}</text>
+                    </binding>
+                </visual>
+            </toast>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            ToastNotification notification = new ToastNotification(doc);
+            ToastNotifier notifier = ToastNotificationManager.CreateToastNotifier();
+            notifier.Show(notification);
+        }
+    }
+}
---
+++ BackgroundTask/ConnectionStatus/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ConnectionStatus
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (BackgroundTaskRegistration.AllTasks.All(x => x.Value.Name != "ConnectionTask"))
+            {
+                BackgroundTaskBuilder taskBuilder = new BackgroundTaskBuilder();
+                taskBuilder.Name = "ConnectionTask";
+                taskBuilder.TaskEntryPoint = "ConnectionStatus.Task.ConnectionTask";
+                taskBuilder.SetTrigger(new SystemTrigger(SystemTriggerType.NetworkStateChange, false));
+                var status = await BackgroundExecutionManager.RequestAccessAsync();
+                if (status != BackgroundAccessStatus.Denied)
+                {
+                    taskBuilder.Register();
+                }
+            }
+        }
+    }
+}
