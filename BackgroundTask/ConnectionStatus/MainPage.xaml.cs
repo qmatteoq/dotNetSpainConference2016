c0a49af16ed8fea2a4c91313379730f0bf03d5bd
Matteo Pagani <info@qmatteoq.com>
2016-02-15 15:12:57 +0100

    Code cleanup

--- BackgroundTask/ConnectionStatus/MainPage.xaml.cs
+++ BackgroundTask/ConnectionStatus/MainPage.xaml.cs
@@ -4,13 +4,8 @@ using Windows.ApplicationModel.Background;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace ConnectionStatus
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
--- ExtendedExecution/MainPage.xaml.cs
+++ ExtendedExecution/MainPage.xaml.cs
@@ -3,18 +3,12 @@ using System.Diagnostics;
 using Windows.ApplicationModel.ExtendedExecution;
 using Windows.Data.Xml.Dom;
 using Windows.Devices.Geolocation;
-using Windows.Storage;
 using Windows.UI.Notifications;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace ExtendedExecution
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
